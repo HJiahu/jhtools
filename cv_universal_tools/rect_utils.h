eca87e95e134f9fc2dd70debf523efc3f7d02626
HJiahu <HJiahu@outlook.com>
2017-12-16 21:58:34 +0800

    update

--- cv_universal_tools/rect_utils.h
+++ cv_universal_tools/rect_utils.h
@@ -1,11 +1,13 @@
 ﻿#ifndef JH_RECT_UTILS_H_
 #define JH_RECT_UTILS_H_
 
+#include<iostream>
 #include<opencv2/opencv.hpp>
+
 
 namespace jhtools
 {
-	//判断rect是否位于img中
+    //判断rect是否位于img中
     inline cv::Rect verify_rect (const cv::Rect& rect, const cv::Mat &img = cv::Mat());
     
     //放大rect，rect = rect*scale，前提是rect依旧位于img内，如果不满足这个要求rect将保持原样并返回false
